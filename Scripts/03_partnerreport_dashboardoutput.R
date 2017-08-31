b4b038dea9bc6ba6abc71dd6f6183324e6fa49ce
Aaron <achafetz@usaid.gov>
2017-08-31 12:22:58 -0400

    updates

--- Scripts/03_partnerreport_dashboardoutput.R
+++ Scripts/03_partnerreport_dashboardoutput.R
@@ -34,7 +34,7 @@
 	names(fvdata) <- tolower(names(fvdata))
 	
 #SNU prioritizations
-	fvdata <- fvdata %>% 
+	df_ppr <- fvdata %>% 
 	  select(-fy16snuprioritization) %>%
     rename(snuprioritization=fy17snuprioritization) %>%
     mutate(snuprioritization = ifelse(is.na(snuprioritization),"[not classified]", snuprioritization))	%>%
@@ -50,63 +50,62 @@
 	         ((standardizeddisaggregate %in% c("MostCompleteAgeDisagg", "Modality/MostCompleteAgeDisagg")) & 
 	         indicator!="HTS_TST_NEG")) %>%
 	
-	 #rename denominator values _D
+	#rename denominator values _D
 	  mutate(indicator = ifelse((indicator=="TB_ART" & disaggregate=="Total Denominator"),"TB_ART_D",indicator), 
-	         indicator = ifelse((indicator=="TB_STAT" & disaggregate=="Total Denominator"),"TB_STAT_D",indicator)) %>%
-
-	 #add future periods if they don't yet exist
-	  mutate(fy2017q4 = 0, fy2017apr = 0) 
-	 
-	 #create cumulative indicator
-	  fvdata[is.na(fvdata)] <- 0
-	  fvdata <- fvdata %>% 
-	      mutate(fy2017cum = ifelse(indicator=="TX_CURR", fy2017q3, 
-	                         ifelse(indicator %in% c("KP_PREV","PP_PREV", "OVC_HIVSTAT", "OVC_SERV", 
-	                                                 "TB_ART", "TB_STAT", "TX_TB", "GEND_GBV", "PMTCT_FO", 
-	                                                 "TX_RET", "KP_MAT"), 
-	                         fy2017q2, fy2017q1 + fy2017q2 + fy2017q3 + fy2017q4))) %>%
-    #create age/sex disagg
-	      mutate(disagg = ifelse(ismcad=="Y", paste(age, sex, sep="/"), "Total"))
-	  
-	  #keep only necessary variables
-	  fvdata <- fvdata %>%  
-	    select(operatingunit, countryname, psnu, psnuuid, snuprioritization,
-	               fundingagency, primepartner, mechanismid, implementingmechanismname,
-	               indicator, disagg, fy2015q2, fy2015q3, fy2015q4, fy2015apr, fy2016_targets,
-	               fy2016q1, fy2016q2, fy2016q2, fy2016q3, fy2016q4, fy2016apr, fy2017_targets,
-	               fy2017q1, fy2017q2, fy2017q3, fy2017q4, fy2017cum)
-	    
+	         indicator = ifelse((indicator=="TB_STAT" & disaggregate=="Total Denominator"),"TB_STAT_D",indicator))
+   
+	 #############################################
 	#replace partner and IM with official names from FACTS Info
-	  df_names <- read_excel(file.path(rawdata,"FY12-16 Standard COP Matrix Report-20170822.xls"), skip = 1)
-	  
-	  #rename variables
-	  names(df_names) <- c("operatingunit", "mechanismid", "primepartner_2014", "implementingmechanismname_2014", 
-	                       "primepartner_2015", "implementingmechanismname_2015", "primepartner_2016", "implementingmechanismname_2016",
-	                       "primepartner_2017", "implementingmechanismname_2017")
-	  #figure out latest name for IM and partner (should both be from the same year)
-	  df_names <- df_names %>%
-	      gather(type, name, primepartner_2014, implementingmechanismname_2014, 
-	              primepartner_2015, implementingmechanismname_2015, primepartner_2016, implementingmechanismname_2016,
-	             primepartner_2017, implementingmechanismname_2017) %>%
-	      separate(type, c("type", "year"), sep="_") %>%
-	      filter(!is.na(name)) %>%
-	      group_by(operatingunit, mechanismid, type) %>%
-        filter(year==max(year)) %>%
-	      ungroup() %>%
-	      spread(type, name) %>%
-	      select(mechanismid, implementingmechanismname, primepartner)
-	      
-
-
+	df_names <- read_excel(file.path(rawdata,"FY12-16 Standard COP Matrix Report-20170822.xls"), skip = 1)
 	
+	#rename variables
+	names(df_names) <- c("operatingunit", "mechanismid", "primepartner_2014", "implementingmechanismname_2014", 
+	                     "primepartner_2015", "implementingmechanismname_2015", "primepartner_2016", "implementingmechanismname_2016",
+	                     "primepartner_2017", "implementingmechanismname_2017")
+	#figure out latest name for IM and partner (should both be from the same year)
+	df_names <- df_names %>%
+	  gather(type, name, primepartner_2014, implementingmechanismname_2014, 
+	         primepartner_2015, implementingmechanismname_2015, primepartner_2016, implementingmechanismname_2016,
+	         primepartner_2017, implementingmechanismname_2017) %>%
+	  separate(type, c("type", "year"), sep="_") %>%
+	  filter(!is.na(name)) %>%
+	  group_by(operatingunit, mechanismid, type) %>%
+	  filter(year==max(year)) %>%
+	  ungroup() %>%
+	  spread(type, name) %>%
+	  select(mechanismid, implementingmechanismname, primepartner) %>%
+	  mutate(mechanismid =  as.character(mechanismid)) %>%
+	  rename(implementingmechanismname_F = implementingmechanismname, primepartner_F = primepartner)
+	  
+	 #merge
+	 df_ppr <- left_join(df_ppr, df_names, by="mechanismid")
+   rm(df_names)
+   
+	 #replace prime partner and mech names
+	 df_ppr <- df_ppr %>%
+	   mutate(implementingmechanismname = ifelse(is.na(implementingmechanismname_F), implementingmechanismname, implementingmechanismname_F), 
+	                  primepartner = ifelse(is.na(primepartner_F), primepartner, primepartner_F)) %>%
+	   select(-ends_with("_F")) %>%
+	 
+	 ###########################################
+	 
+   #create age/sex disagg
+	  mutate(disagg = ifelse(ismcad=="Y", paste(age, sex, sep="/"), "Total")) %>%
+	  
+  #aggregate by subset variable list
+  group_by(operatingunit, countryname, psnu, psnuuid, snuprioritization,
+                  fundingagency, primepartner, mechanismid, implementingmechanismname,
+                  indicator, disagg) %>%
+   summarize_at(vars(starts_with("fy")), funs(sum(., na.rm=TRUE))) %>%
+   ungroup()
+
+#####
+ 
 #TX_NET_NEW indicator
 	#subset to just TX_CURR which will become NET NEW
-	df_netnew <- fvdata %>%
+	df_netnew <- df_ppr %>%
 	  filter(indicator=="TX_CURR") %>%
 	  mutate(indicator = "TX_NET_NEW") 
-	
-	#replace all NAs with zero to calculate 
-	df_netnew[is.na(df_netnew)] <- 0
 	
 	#create net new variable
 	df_netnew <- df_netnew %>%
@@ -117,27 +116,52 @@
 	         fy2017q2_cc =  fy2017q2 - fy2017q1,
 	         fy2017q3_cc =  fy2017q3 - fy2017q2,
 	         fy2017_targets_cc = fy2017_targets - fy2016q4) %>%
-	  mutate(fy2015q2 = NA, 
-	         fy2015q3 = NA,
+	  mutate(fy2015q2 = 0, 
+	         fy2015q3 = 0,
 	         fy2015q4 =  fy2015q4_cc,
-	         fy2015apr = NA,
-	         fy2016_targets = NA,
-	         fy2016q1 =  NA,
+	         fy2015apr = 0,
+	         fy2016_targets = 0,
+	         fy2016q1 =  0,
 	         fy2016q2 =  fy2016q2_cc,
-	         fy2016q3 =  NA,
+	         fy2016q3 =  0,
 	         fy2016q4 =  fy2016q4_cc,
 	         fy2016apr = fy2016q2_cc + fy2016q4_cc, 
 	         fy2017q1 =  fy2017q1_cc,
 	         fy2017q2 =  fy2017q2_cc,
 	         fy2017q3 =  fy2017q3_cc,
 	         fy2017_targets = fy2017_targets_cc) %>%
-	  select(-ends_with("_cc"))
-	
-	df_netnew[df_netnew==0] <- NA
+	  select(-ends_with("_cc")) %>%  
 	  
+	  #append TX_NET_NEW onto main dataframe
+	  bind_rows(df_netnew)
+	  rm(df_netnew)
+	
+#####   
 
+	#create cumulative indicator
+	df_ppr[is.na(df_ppr)] <- 0
+	df_ppr <- df_ppr %>% 
+	  mutate(fy2017cum = ifelse(indicator=="TX_CURR", fy2017q3, 
+	                            ifelse(indicator %in% c("KP_PREV","PP_PREV", "OVC_HIVSTAT", "OVC_SERV", 
+	                                                    "TB_ART", "TB_STAT", "TX_TB", "GEND_GBV", "PMTCT_FO", 
+	                                                    "TX_RET", "KP_MAT"), 
+	                                   fy2017q2, fy2017q1 + fy2017q2 + fy2017q3 + fy2017q4)))
 	
 	
+	df_ppr[df_ppr==0] <- NA
+	
+	df_ppr <- df_ppr %>%
+	    gather(period, value, contains("fy")) %>%
+	    drop_na(value) %>%
+	    spread(period, value) %>%
+	  #add future periods if they don't yet exist
+	  mutate(fy2017q4 = NA) %>%
+	    select(operatingunit, countryname, psnu, psnuuid, snuprioritization,
+	           fundingagency, primepartner, mechanismid, implementingmechanismname,
+	           indicator, disagg, fy2015q2, fy2015q3, fy2015q4, fy2015apr, fy2016_targets,
+	           fy2016q1, fy2016q2, fy2016q2, fy2016q3, fy2016q4, fy2016apr, fy2017_targets,
+	           fy2017q1, fy2017q2, fy2017q3, fy2017q4, fy2017cum)
+	    
 	
 	
-	
+	test<- df_ppr %>% drop_na(fy2015q2:fy2017cum)
