0f5ad2b415aa07560f4875d9c2c9fae279a6003e
achafetz <achafetz@usaid.gov>
2018-03-20 15:41:31 -0400

    find current period function

---
+++ Scripts/currentperiod.R
@@ -0,0 +1,37 @@
+#' Extract lastest period of dataset
+#'
+#' @param df dataset to use to find latest period 
+#' @param pd_type what is returned? (a) full, eg fy2018q1; (b)year, eg 2018; (c) or quarter, eg 1
+#'
+#' @return last column/year/quarter
+#'
+#' @importFrom dplyr %>%
+#' @importFrom stringr everything
+#'
+#' @examples
+#' currentpd(df_mer)
+#' currrentpd(df_mer, "quarter")
+#' 
+currentpd <- function(df, pd_type = "full") {
+  #get list of headers
+  headers <- names(df)
+  #figure out column, keeping only variables that are a quarter
+  pd <- headers[stringr::str_detect(headers, "q(?=[:digit:])")] %>% 
+    tail(., n =1)
+  #extract different poritions of the the last column based on pd_type 
+  if(pd_type == "year") {
+    start_pt <- 3 
+    end_pt <- -3
+  } else if(pd_type == "quarter") {
+    start_pt <- -1 
+    end_pt <- -1
+  } else {
+    return(pd)
+    break
+  }
+  #for year/quarter, extract portion asked for and return as integer
+  pd <- stringr::str_sub(pd, start_pt, end_pt) %>% 
+    as.integer(.)
+  return(pd)
+}
+
---
+++ Scripts/futurefiller.R
@@ -0,0 +1,33 @@
+#' Create/Fill future period columns with zero as placeholders
+#'
+#' @param df dataset to add future columns to
+#'
+#' @importFrom dplyr everything
+#' 
+#' @examples
+#' df_mer <- fill_future_pds(df_mer)
+#' 
+fill_future_pds <- function(df){
+  
+  #find current quarter
+  source(here("Scripts", "currentpd.R"))
+  curr_q <- currentperiod(df, "quarter")
+  
+  #add new columns if not yet at q4  
+  if(curr_q != 4) {
+    #n+1 quater 
+    new_qs <- curr_q + 1 
+    #create new columns for n+1 quater to Q4
+    for(i in new_qs:4){
+      #define variable name, eg fy2018q2
+      varname <- paste0(curr_fy, "q", i)
+      #create variable
+      df <- df %>% 
+        dplyr::mutate(!!varname := 0)
+    }
+    #create apr column
+    varname <- paste0(curr_fy, "apr")
+    df <- df %>% 
+      dplyr::mutate(!!varname := 0)
+  }
+}
