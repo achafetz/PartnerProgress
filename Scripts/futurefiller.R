247b6b616f2d3d32d1bf2fbc32707c03c44ab473
achafetz <achafetz@usaid.gov>
2018-03-21 09:05:20 -0400

    add fy and qtr (and change q to qtr)

--- Scripts/futurefiller.R
+++ Scripts/futurefiller.R
@@ -2,23 +2,23 @@
 #'
 #' @param df dataset to add future columns to
 #' @param fy current fiscal year
-#' @param q current quarter
+#' @param qtr current quarter
 #'
 #' @importFrom dplyr everything
 #' 
 #' @examples
-#' df_mer <- fill_future_pds(df_mer)
+#' df_mer <- fill_future_pds(df_mer, 2018, 1)
 #' 
-fill_future_pds <- function(df, fy, q){
+fill_future_pds <- function(df, fy, qtr){
 
-  #add new columns if not yet at q4  
-  if(q != 4) {
-    #n+1 quater 
-    new_qs <- q + 1 
+  #add new columns if not yet at qtr4  
+  if(qtr != 4) {
+    #n+1 qtruater 
+    new_qtrs <- qtr + 1 
     #create new columns for n+1 quater to Q4
-    for(i in new_qs:4){
+    for(i in new_qtrs:4){
       #define variable name, eg fy2018q2
-      varname <- paste0("fy", fy, "q", i)
+      varname <- paste0("fy", fy, "qtr", i)
       #create variable
       df <- df %>% 
         dplyr::mutate(!!varname := 0)
