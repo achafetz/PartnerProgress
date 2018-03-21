22e171e9bde22d997baea2642f5c6c086ada100c
achafetz <achafetz@usaid.gov>
2018-03-21 11:37:17 -0400

    recreate the folder initialization as a function; remove pacakages and assignments

--- Scripts/00_partnerreport_initiatalize.R
+++ Scripts/00_partnerreport_initiatalize.R
@@ -4,35 +4,37 @@
 # Adapted from T. Essam, USAID
 # Updated: 8/31/17 
 
+#' Initialize folder structure with sub folders
+#'
+#' @details 
+#' Adapted from T. Essam, USAID [Stata]
+#' @param projectname name of the project
+#' @param projectpath path to the folder where you want your project to sit
+#' @param ... subfolders to add in list, c("x", "y")
+#'
+#' @examples
+#' initialize_fldr("NewProject", "~/GitHub", "RawData", "Documents", "Scripts", "Output")
 
-## DEPENDENT PACKAGES ##
-  lib <- c("plyr", "tidyverse", "readxl", "readr", "ggplot2", "foreign")
-  lapply(lib, require, character.only = TRUE)
+initialize_fldr <- function(projectname, projectpath, ...){
 
 ## FILE PATHS ##
   #  must be run each time R project is opened
   # Choose the project path location to where you want the project parent folder to go on your machine.
-    projectpath <- "C:/Users/achafetz/Documents/GitHub"
     setwd(projectpath)
 
-  # Run a macro to set up study folder
-    pfolder <- "PartnerProgress" # Name the folder name here
-    dir.create(file.path(pfolder), showWarnings = FALSE)
-    setwd(file.path(projectpath, pfolder))
+  # Set up project folder
+    dir.create(file.path(projectname), showWarnings = FALSE)
+    setwd(file.path(projectpath, projectname))
 
   # Run initially to set up folder structure
-  # Choose your folders to createa and and stored as values
-    folderlist <- c("RawData", "Documents", "Scripts", "ExcelOutput")
+    folderlist <- list(...)
     for (f in folderlist){
-      dir.create(file.path(projectpath, pfolder, f))
-      assign(tolower(f), file.path(projectpath, pfolder, f,"/"))
+      dir.create(file.path(projectpath, projectname, f))
+      #assign(tolower(f), file.path(projectpath, projectname, f))
     }
   
-  # additional folders outside of project folder (due to large file size)
-    datafv <- "C:/Users/achafetz/Documents/ICPI/Data/"
+  # assign additional folders outside of project folder (due to large file size)
+    #datafv <- "C:/Users/achafetz/Documents/ICPI/Data/"
 
-  #clean up stored values
-    rm(lib, projectpath, pfolder, folderlist, f)
-    
-    
+}   
     
