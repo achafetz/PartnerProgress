c0433ac739e2bfce86eca27991c59ceb3b71edc0
achafetz <achafetz@usaid.gov>
2018-03-26 16:28:03 -0400

    add save period

--- Scripts/export.R
+++ Scripts/export.R
@@ -2,6 +2,7 @@
 #'
 #' @param df dataframe to export
 #' @param opunit which operatingunit should an output be created for?
+#' @param savepd period to add to file name, eg FY2018Q1
 #'
 #' @return
 #' @export 
@@ -9,13 +10,13 @@
 #' @importFrom dplyr %>%
 #'
 #' @examples
-#'  export(df_mer, "Kenya")
+#'  export(df_mer, "Kenya", "FY2018Q1")
 #'  
-export <- function(df, opunit){
+export <- function(df, opunit, savepd){
 
   print(paste("export dataset:", opunit))
   filename <- 
-    paste0("PPRdata_", opunit,"_", fy_save, ".csv") 
+    paste0("PPRdata_", opunit,"_", savepd, ".csv") 
   
   if(stringr::str_detect(opunit, "GLOBAL") == FALSE) {
     df %>% 
