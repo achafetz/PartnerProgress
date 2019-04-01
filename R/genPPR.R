bc9ac7e8a085e054fa0dac3ff8a2920cb07fa024
Aaron Chafetz <achafetz@usaid.gov>
2019-04-01 13:48:45 -0400

    remove FY19Q1i fix for pos

--- R/genPPR.R
+++ R/genPPR.R
@@ -44,13 +44,6 @@ genPPR <- function(folderpath_msd, output_global = TRUE, output_ctry_all = TRUE,
   	curr_fy <- currentpd(df_mer, "year")
   	fy_save <- currentpd(df_mer, "full") %>%
   	           toupper()
-  
-  ## FOR FY19Q1i only -> FY19 Targets for TB_STAT_POS and PMTCT_STAT_POS missing from MSD
-  	if (fy_save == "FY2019Q1"){
-  	  df_mer <- df_mer %>% 
-  	    add_pos("PMTCT_STAT") %>% 
-  	    add_pos("HTS_TST")
-  	}
   	
   #add MCAD variable for FY18 (only present prior to FY18)
   	df_ppr <- add_mcad(df_mer)
