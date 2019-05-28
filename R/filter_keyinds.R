bb95aac926a107013d612b79eed7bb723da73d3a
Aaron Chafetz <achafetz@usaid.gov>
2019-05-28 16:02:45 -0400

    adjust disaggs filtering since no more MCAD

--- R/filter_keyinds.R
+++ R/filter_keyinds.R
@@ -10,7 +10,7 @@
 #' 
 
 key_ind <- function(qtr){
-  #q1 (NET_NEW will be created, so not in list)
+  #q1
   ind <- c("HTS_TST", "TX_NEW", "PMTCT_EID", "HTS_TST_POS", "PMTCT_STAT", 
            "PMTCT_STAT_POS", "TX_CURR", "PMTCT_ART", "VMMC_CIRC", "TX_NET_NEW",
            "TB_ART", "TB_STAT", "TB_STAT_POS", "TB_ART_D", "TB_STAT_D",
@@ -49,19 +49,26 @@ filter_keyinds <- function(df, qtr){
   #create new indicator variable for only the ones of interest for analysis
   #rename denominator values _D
   df <- df %>% 
-    dplyr::mutate(indicator = ifelse((indicator=="TB_ART" & standardizeddisaggregate=="Total Denominator"),"TB_ART_D",indicator),
-                  indicator = ifelse((indicator=="TB_STAT" & standardizeddisaggregate=="Total Denominator"),"TB_STAT_D",indicator),
-                  standardizeddisaggregate = ifelse((indicator %in% c("TB_ART_D", "TB_STAT_D")),"Total Numerator",standardizeddisaggregate)
-                  )
+    dplyr::mutate(indicator = ifelse(indicator %in% c("TB_ART","TB_STAT") &  standardizeddisaggregate=="Total Denominator", paste0(indicator, "_D"), indicator),
+                  standardizeddisaggregate = ifelse((indicator %in% c("TB_ART_D", "TB_STAT_D")),"Total Numerator",standardizeddisaggregate))
   
   #indicators to keep (based on the current quarter)
    ind_list <- key_ind(qtr)
   
   #filter to select indicators (based on quarter)
   df_keyind <- df %>% 
-    dplyr::filter(((indicator %in% ind_list) & standardizeddisaggregate=="Total Numerator") |
-                    ((standardizeddisaggregate %in% c("MostCompleteAgeDisagg", "Modality/MostCompleteAgeDisagg")) & 
-                       indicator!="HTS_TST_NEG") & sex!="" & (agecoarse %in% c("<15", "15+")))
+    dplyr::filter(indicator %in% ind_list,
+                  standardizeddisaggregate=="Total Numerator")
+  
+  #filter a secondary df that contains age/sex disaggs for select in available quarterly
+  df_keyind_disaggs <- df %>% 
+    dplyr::filter(dplyr::filter(indicator %in% c("HTS_TST", "HTS_TST_POS", "TX_NEW", "TX_CURR", "TX_NET_NEW"),
+                                stringr::str_detect(standardizeddisaggregate, "Sex"),
+                                sex!="",
+                                trendscoarse %in% c("<15", "15+")))
+  
+  #join
+  df_keyind <- dplyr::bind_rows(df_keyind,df_keyind_disaggs)
   
   return(df_keyind)
 }
